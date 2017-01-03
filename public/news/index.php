cab2eb2865d5f90cf6d3e5434c544247c84fd97c
root <root@slaptech-ubuntu-2gb-sfo2-01.localdomain>
2017-01-03 08:58:38 +0000

    add index scripts in writable dirs

---
+++ public/news/index.php
@@ -0,0 +1,17 @@
+<?php
+// Paging newswire & search
+
+include_once("shared/global.cfg");
+
+$page = new Page('newswire');
+if ($page->get_error())
+{
+    echo "Fatal error: " . $page->get_error();
+}
+else
+{
+    $page->build_page();
+    sf_include_file(SF_INCLUDE_PATH, 'content-header.inc');
+    echo $page->get_html();
+    sf_include_file(SF_INCLUDE_PATH, 'content-footer.inc');
+}
---
+++ public/syn/index.php
@@ -0,0 +1,20 @@
+<?php
+//This page gives an overview of the different syndication files (per category). It is served from cache to avoid dbase overload.
+
+$display=true;
+include("shared/global.cfg");
+sf_include_file (SF_INCLUDE_PATH, 'content-header.inc');
+
+$page = new Page('syndication_index');
+
+if ($page->get_error())
+{
+    echo "Fatal error: " . $page->get_error();
+} else
+{
+    $page->build_page('syndication_index');
+    echo $page->get_html();
+}
+
+sf_include_file (SF_INCLUDE_PATH, 'content-footer.inc');
+
